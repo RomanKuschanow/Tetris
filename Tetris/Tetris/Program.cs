a5bac0f431f4a5bff9fcc4ec92a8a60765110cdd
RomanKuschanow <kuschanow@gmail.com>
2021-02-07 23:38:43 +0200

    HandleKey

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -11,25 +11,31 @@ namespace Tetris
             Console.SetBufferSize(40, 30);
 
             FigureGenerator generator = new FigureGenerator(20, 0, '*');
-            Figure s = null;
+            Figure currentFigure = generator.GetNewFigure();
 
             while (true)
             {
-                FigureFall(s, generator);
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    HandleKey(currentFigure, key);
+                }
             }
         }
-        
-        static void FigureFall(Figure fig, FigureGenerator generator)
-        {
-            fig = generator.GetNewFigure();
-            fig.Draw();
 
-            for (int i = 0; i < 15; i++)
+        private static void HandleKey(Figure currentFigure, ConsoleKeyInfo key)
+        {
+            switch (key.Key)
             {
-                fig.Hide();
-                fig.Move(Direction.DOWN);
-                fig.Draw();
-                Thread.Sleep(200);
+                case ConsoleKey.LeftArrow:
+                    currentFigure.Move(Direction.LEFT);
+                    break;
+                case ConsoleKey.RightArrow:
+                    currentFigure.Move(Direction.RIGHT);
+                    break;
+                case ConsoleKey.DownArrow:
+                    currentFigure.Move(Direction.DOWN);
+                    break;
             }
         }
     }
